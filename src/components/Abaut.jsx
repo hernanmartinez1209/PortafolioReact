b6d49f34004ab7c885f5c71a1c970ab17ede87e5
hernanmartinez1209 <108160168+hernanmartinez1209@users.noreply.github.com>
2022-11-06 21:13:51 -0600

    commit final

--- src/components/Abaut.jsx
+++ src/components/Abaut.jsx
@@ -9,7 +9,7 @@ const Abaut = () => {
       </h4>
       <img
         className="img__about"
-        src="https://scontent.ftgu1-2.fna.fbcdn.net/v/t1.6435-9/45588434_1866201000144639_6681716786011832320_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_eui2=AeHDj1qXImNAftggk-6_zENC1_t1FJBgqVrX-3UUkGCpWq-vBEnLjnMQYFo3GjAfYgqyW9L2pXuDqAEXlXE6N-np&_nc_ohc=ofz70aWp61QAX-Lzhfp&_nc_ht=scontent.ftgu1-2.fna&oh=00_AfA2967ChfS8Dkzyp9Z3GAqjnWhA2mt5xmst1UWocx2YLw&oe=63891817"
+        src="./imgs/fotoPersonal.jpeg"
         alt="Photo__personal"
       />
       <article className="info__About">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
         <div className='Hedaer'>
             <h2>hi there</h2>
             <h1>i am a Web Designer</h1>
-            <img className='Headaer__img' src="https://scontent.ftgu1-2.fna.fbcdn.net/v/t1.6435-9/45588434_1866201000144639_6681716786011832320_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=174925&_nc_eui2=AeHDj1qXImNAftggk-6_zENC1_t1FJBgqVrX-3UUkGCpWq-vBEnLjnMQYFo3GjAfYgqyW9L2pXuDqAEXlXE6N-np&_nc_ohc=ofz70aWp61QAX-Lzhfp&_nc_ht=scontent.ftgu1-2.fna&oh=00_AfA2967ChfS8Dkzyp9Z3GAqjnWhA2mt5xmst1UWocx2YLw&oe=63891817" alt="Photo__personal" />
+            <img className='Headaer__img' src="./imgs/fotoPersonal.jpeg" alt="Photo__personal" />
             <p className='p__Header' >!Ven y Creemos sitios expectaculares Contactame!</p>
             {/* <button className='btn__Header'>More About Me</button> */}
             <article className='box__info-header'>
